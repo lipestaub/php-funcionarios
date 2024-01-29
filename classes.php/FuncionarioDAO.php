5b1de83adc27e59db1997726cd92164093ed066c
Felipe <lipestaub@gmail.com>
2024-01-29 18:05:49 -0300

    update

--- classes.php/FuncionarioDAO.php
+++ classes.php/FuncionarioDAO.php
@@ -4,33 +4,58 @@
     class FuncionarioDAO {
         public function create(Funcionario $funcionario) {
             $connection = Conexao::getConnection();
-            $stmt = $connection->prepare("INSERT INTO funcionarios VALUES(:id, :nome, :genero, :idade, :salario);");
-            $stmt->bindParam('id', $funcionario->getId());
-            $stmt->bindParam('nome', $funcionario->getNome());
-            $stmt->bindParam('genero', $funcionario->getGenero());
-            $stmt->bindParam('idade', $funcionario->getIdade());
-            $stmt->bindParam('salario', $funcionario->getSalario());
+            $stmt = $connection->prepare("INSERT INTO funcionarios (nome, genero, idade, salario) VALUES(:nome, :genero, :idade, :salario);");
+            $stmt->bindValue(':nome', $funcionario->getNome());
+            $stmt->bindValue(':genero', $funcionario->getGenero());
+            $stmt->bindValue(':idade', $funcionario->getIdade());
+            $stmt->bindValue(':salario', $funcionario->getSalario());
             $stmt->execute();
         }
 
         public function update(Funcionario $funcionario) {
-            
+            $connection = Conexao::getConnection();
+            $stmt = $connection->prepare("UPDATE funcionarios SET nome = :nome, genero = :genero, idade = :idade, salario = :salario WHERE id = :id;");
+            $stmt->bindValue(':id', $funcionario->getId());
+            $stmt->bindValue(':nome', $funcionario->getNome());
+            $stmt->bindValue(':genero', $funcionario->getGenero());
+            $stmt->bindValue(':idade', $funcionario->getIdade());
+            $stmt->bindValue(':salario', $funcionario->getSalario());
+            $stmt->execute();
         }
 
         public function updateSalario(int $id, int $percentual) {
-            
+            $percentual = 1 + ($percentual / 100);
+            $connection = Conexao::getConnection();
+            $stmt = $connection->prepare("UPDATE funcionarios SET salario = salario * :percentual WHERE id = :id;");
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':percentual', $percentual);
+            $stmt->execute();
         }
 
         public function delete(int $id) {
-            
+            $connection = Conexao::getConnection();
+            $stmt = $connection->prepare("DELETE FROM funcionarios WHERE id = :id;");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
         }
 
         public function getFuncionarios() {
-            
+            $connection = Conexao::getConnection();
+            $stmt = $connection->prepare("SELECT * FROM funcionarios;");
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            return $stmt->fetchAll();
         }
 
         public function getFuncionario(int $id) {
-            
+            $connection = Conexao::getConnection();
+            $stmt = $connection->prepare("SELECT * FROM funcionarios WHERE id = :id;");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            return $stmt->fetchAll();
         }
     }
 ?>
--- index.php
+++ index.php
@@ -5,34 +5,53 @@
     $funcionarioDAO = new FuncionarioDAO();
 
     $funcionarios = [
-        $f1 = new Funcionario('Felipe', 'M', 20, 1500),
-        $f2 = new Funcionario('Luan', 'M', 20, 750.50),
-        $f3 = new Funcionario('Rafael', 'M', 20, 1200),
-        $f4 = new Funcionario('Gustavo', 'M', 20, 980.98)
+        new Funcionario('Felipe', 'M', 20, 1500),
+        new Funcionario('Luan', 'M', 20, 750.50),
+        new Funcionario('Rafael', 'M', 20, 1200),
+        new Funcionario('Gustavo', 'M', 20, 980.98)
     ];
 
     foreach ($funcionarios as $funcionario) {
         $funcionarioDAO->create($funcionario);
     }
-/*
-    $funcionarios = $funcionarioDAO->getFuncionarios();
+
+    echo "CADASTROS REALIZADOS\n";
+
+    $funcionariosDB = $funcionarioDAO->getFuncionarios();
+
+    echo "FUNCIONÁRIOS:\n";
+    print_r($funcionarios);
+
+    foreach ($funcionariosDB as $key=>$funcionario) {
+        $funcionarios[$key]->setId($funcionariosDB[$key]['id']); 
+    }
+
+    echo "IDS SETADOS\n";
 
     $funcionarios[2]->setNome('Pedro');
     $funcionarioDAO->update($funcionarios[2]);
+
+    echo "O FUNCIONÁRIO " . $funcionarios[2]->getId() . " FOI ALTERADO\n";
 
     $funcionarioDAO->updateSalario($funcionarios[2]->getId(), 10);
 
     $funcionarios = $funcionarioDAO->getFuncionarios();
 
+    echo "FUNCIONÁRIOS:\n";
     print_r($funcionarios);
 
-    $idFuncionario = $funcionarios[2]->getId();
+    $idFuncionario = $funcionarios[2]['id'];
 
     $funcionario = $funcionarioDAO->getFuncionario($idFuncionario);
 
+    echo "FUNCIONÁRIO: " . $idFuncionario . "\n";
+
     print_r($funcionario);
 
-    $funcionarioDAO->delete($funcionarios[1]->getId());
+    $funcionarioDAO->delete($idFuncionario);
 
-    print_r($funcionarioDAO->getFuncionarios());*/
+    echo "O FUNCIONÁRIO " . $idFuncionario . " FOI EXCLUÍDO\n";
+
+    echo "FUNCIONÁRIOS:\n";
+    print_r($funcionarioDAO->getFuncionarios());
 ?>
